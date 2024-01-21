fecc049f4024b9209dadc23a54cf75fd775bd8dc
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2024-01-21 17:13:58 +0900

    numA、numBが使われていた一部をbirthdayA, birthdayBに変更

--- chap04/twoBirthday.js
+++ chap04/twoBirthday.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var numA = Math.round(Math.random() * 68 + 1950);
 var birthdayA = numA;
-console.log("A\u3055\u3093\u306E\u751F\u307E\u308C\u5E74\u306F\u3001".concat(numA, "\u5E74\u3067\u3059\u3002"));
+console.log("A\u3055\u3093\u306E\u751F\u307E\u308C\u5E74\u306F\u3001".concat(birthdayA, "\u5E74\u3067\u3059\u3002"));
 var numB = Math.round(Math.random() * 68 + 1950);
 var birthdayB = numB;
-console.log("B\u3055\u3093\u306E\u751F\u307E\u308C\u5E74\u306F\u3001".concat(numB, "\u5E74\u3067\u3059\u3002"));
-if (numA >= 1989 && numB >= 1989) {
+console.log("B\u3055\u3093\u306E\u751F\u307E\u308C\u5E74\u306F\u3001".concat(birthdayB, "\u5E74\u3067\u3059\u3002"));
+if (birthdayA >= 1989 && birthdayB >= 1989) {
     console.log("AさんもBさんも平成生まれ");
 }
-else if (numA >= 1989 || numB >= 1989) {
+else if (birthdayA >= 1989 || birthdayB >= 1989) {
     console.log("どちらかが平成生まれ");
 }
 else {
--- chap04/twoBirthday.ts
+++ chap04/twoBirthday.ts
@@ -2,14 +2,14 @@ export{}
 
 const numA = Math.round(Math.random() * 68 + 1950);
 const birthdayA = numA;
-console.log(`Aさんの生まれ年は、${numA}年です。`);
+console.log(`Aさんの生まれ年は、${birthdayA}年です。`);
 const numB = Math.round(Math.random() * 68 + 1950);
 const birthdayB = numB;
-console.log(`Bさんの生まれ年は、${numB}年です。`);
+console.log(`Bさんの生まれ年は、${birthdayB}年です。`);
 
-if(numA >= 1989 && numB >= 1989) {
+if(birthdayA >= 1989 && birthdayB >= 1989) {
     console.log("AさんもBさんも平成生まれ");
-} else if(numA >= 1989 || numB >= 1989) {
+} else if(birthdayA >= 1989 || birthdayB >= 1989) {
     console.log("どちらかが平成生まれ");
 } else {
     console.log("両方とも昭和生まれ");
