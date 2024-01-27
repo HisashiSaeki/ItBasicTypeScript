0ee4838d90afc0c65ca113b98720688fff3614e7
HisashiSaeki <143866672+HisashiSaeki@users.noreply.github.com>
2024-01-27 11:16:37 +0900

    変数名の修正

--- chap08/showVolumes.js
+++ chap08/showVolumes.js
@@ -1,16 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function calcVolume(num1, num2, num3) {
-    var area = 0;
-    if (typeof num2 == "number" && typeof num3 == "number") {
-        area = num1 * num2 * num3;
+function calcVolume(edge, height, depth) {
+    var volume = 0;
+    if (typeof height == "number" && typeof depth == "number") {
+        volume = edge * height * depth;
     }
     else {
-        area = Math.pow(num1, 3);
+        volume = Math.pow(edge, 3);
     }
-    return area;
+    return volume;
 }
-var result1 = calcVolume(4);
-console.log("\u4E00\u8FBA\u304C4\u306E\u7ACB\u65B9\u4F53\u306E\u4F53\u7A4D: ".concat(result1));
-var result2 = calcVolume(4, 5, 6);
-console.log("\u5404\u8FBA\u306E\u9577\u3055\u304C4\u30015\u30016\u306E\u76F4\u65B9\u4F53\u306E\u4F53\u7A4D: ".concat(result2));
+var volumeCube = calcVolume(4);
+console.log("\u4E00\u8FBA\u304C4\u306E\u7ACB\u65B9\u4F53\u306E\u4F53\u7A4D: ".concat(volumeCube));
+var volumeCuboid = calcVolume(4, 5, 6);
+console.log("\u5404\u8FBA\u306E\u9577\u3055\u304C4\u30015\u30016\u306E\u76F4\u65B9\u4F53\u306E\u4F53\u7A4D: ".concat(volumeCuboid));
--- chap08/showVolumes.ts
+++ chap08/showVolumes.ts
@@ -1,18 +1,18 @@
 export{}
 
-function calcVolume(num1: number): number;
-function calcVolume(num1: number, num2: number, num3: number): number;
-function calcVolume(num1: number, num2?: number, num3?: number): number {
-  let area = 0;
-  if(typeof num2 == "number" && typeof num3 == "number") {
-    area = num1 * num2 * num3;
+function calcVolume(edge: number): number;
+function calcVolume(width: number, height: number, depth: number): number;
+function calcVolume(edge: number, height?: number, depth?: number): number {
+  let volume = 0;
+  if(typeof height == "number" && typeof depth == "number") {
+    volume = edge * height * depth;
   } else {
-    area = num1 ** 3;
+    volume = edge ** 3;
   }
-  return area
+  return volume
 }
 
-const result1 = calcVolume(4);
-console.log(`一辺が4の立方体の体積: ${result1}`);
-const result2 = calcVolume(4, 5, 6);
-console.log(`各辺の長さが4、5、6の直方体の体積: ${result2}`);
+const volumeCube = calcVolume(4);
+console.log(`一辺が4の立方体の体積: ${volumeCube}`);
+const volumeCuboid = calcVolume(4, 5, 6);
+console.log(`各辺の長さが4、5、6の直方体の体積: ${volumeCuboid}`);
